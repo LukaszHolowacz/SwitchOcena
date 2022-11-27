b8a87d018d207c00af00f3bd0822a41e4d08a3f4
Lukas <lukasz.holowacz5@gmail.com>
2022-11-27 20:15:15 +0100

    Skończona aplikacja

--- app/src/main/java/com/example/switchocena/MainActivity.kt
+++ app/src/main/java/com/example/switchocena/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.switchocena
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,12 +9,11 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var liczby: List<Int>
+        val do_sortowania: MutableList<Int> = mutableListOf()
         val sort = findViewById<Button>(R.id.sortowanie)
         val los = findViewById<Button>(R.id.losowanie)
         val switche = listOf<Switch>(
@@ -41,28 +39,69 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.text9)
         )
 
-        for(i in 0..switche.size-1){
-            switche[i].setOnClickListener {
-
-            }
-        }
+        przypisywanie_wartosci(switche, teksty)
 
         los.setOnClickListener {
-            liczby = random_numbers()
-            for(i in 0..switche.size-1){
-                teksty[i].text = liczby[i].toString()
+            if(!do_sortowania.isEmpty()){
+                do_sortowania.clear()
+            }
+            przypisywanie_wartosci(switche, teksty)
+        }
+
+        sort.setOnClickListener {
+            if(!do_sortowania.isEmpty()){
+                do_sortowania.clear()
+            }
+            if(teksty[0].text != ""){
+                for(i in 0..switche.size-1){
+                    if(switche[i].isChecked){
+                        do_sortowania.add(teksty[i].text.toString().toInt())
+                    }
+                }
+                if(!do_sortowania.isEmpty()){
+                    val posortowana_lista = sortowanie_babelkowe(do_sortowania, do_sortowania.size-1)
+                    findViewById<TextView>(R.id.wynik).text = posortowana_lista.toString()
+                }
+                else{
+                    Toast.makeText(applicationContext, "Musisz coś zaznaczyć, żeby to posortować!", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(applicationContext, "Najpierw musisz wylosować liczby!", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    fun random_numbers(): List<Int>{
-    val randomValues: List<Int> = emptyList()
+    fun random_numbers(): MutableList<Int>{
+    val randomValues: MutableList<Int> = emptyList<Int>().toMutableList()
         while(randomValues.size < 9){
-            var rnd = Random.nextInt(0,9)
-            if(!randomValues.contains(rnd)){
-                //
+            val rnd = Random.nextInt(1,10)
+            if(!randomValues.contains(rnd)) {
+                randomValues.add(rnd)
             }
         }
         return randomValues
+    }
+
+    fun przypisywanie_wartosci(lista1: List<Switch>, lista2: List<TextView>){
+        val liczby = random_numbers()
+        for(i in 0..lista1.size-1){
+            lista1[i].isChecked = false
+            lista2[i].text = liczby[i].toString()
+        }
+    }
+
+    fun sortowanie_babelkowe(tab: MutableList<Int>, size: Int): MutableList<Int>{
+        var pom: Int
+        for(i in 0..size){
+            for (j in 1..size){
+                if(tab[j-1]>tab[j]){
+                    pom = tab[j]
+                    tab[j] = tab[j-1]
+                    tab[j-1] = pom
+                }
+            }
+        }
+        return tab
     }
 }
